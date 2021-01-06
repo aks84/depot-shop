e330bccfb3b7f056e04eee9bc670e4c92832d029
Amulya Kumar Shahi <amulyaa_24@yahoo.in>
2021-01-06 06:49:50 +0530

    add products to cart working now

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,4 +1,6 @@
 class LineItemsController < ApplicationController
+  include CurrentCart
+  before_action :set_cart, only: [:create]
   before_action :set_line_item, only: [:show, :edit, :update, :destroy]
 
   # GET /line_items
@@ -21,12 +23,20 @@ class LineItemsController < ApplicationController
 
   # POST /line_items
   def create
-    @line_item = LineItem.new(line_item_params)
+    product = Product.find(params[:product_id])
+    @line_item = @cart.line_items.build(product: product)
 
-    if @line_item.save
-      redirect_to @line_item, notice: 'Line item was successfully created.'
-    else
-      render :new
+    respond_to do |format|
+      if @line_item.save
+        format.html {redirect_to @line_item.cart,
+          notice: "Line item was successfully created."}
+        format.json { render :show,
+          status: :created, location: @line_item }
+      else
+        format.html { render :new }
+        format.json { render json: @line_item.errors,
+          status: :unprocessably_entity }
+      end
     end
   end
 
